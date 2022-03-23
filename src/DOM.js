37398e9e9ca32248f3e0af3455684a29fd66fc63
Tanya <70838088+tkubysheva@users.noreply.github.com>
2022-03-23 20:02:23 +0700

    Задание dom

--- src/DOM.js
+++ src/DOM.js
@@ -5,6 +5,14 @@
   Считаем, что всегда передается тег, допускающий вставку текста в качестве своего содержимого (P, DIV, I и пр.).
 */
 export function appendToBody(tag, content, count) {
+    for (let i = 0; i < count; ++i) {
+        document
+            .getElementsByTagName('body')[0]
+            .insertAdjacentHTML(
+                'beforeend',
+                '<' + tag + '>' + content + '</' + tag + '>',
+            );
+    }
 }
 
 /*
@@ -14,7 +22,22 @@ export function appendToBody(tag, content, count) {
   Каждый элемент должен иметь класс вида item_n, где n - глубина вложенности элемента. (Нумерацию ведем с единицы).
   Сформированное дерево верните в качестве результата работы функции.
 */
+
+function createRoot(prevDiv, childrenCount, currentLevel, finalLevel) {
+    for (let i = 0; i < childrenCount; ++i) {
+        const newDiv = document.createElement('div');
+        newDiv.className = 'item_' + currentLevel;
+        prevDiv.insertAdjacentElement('beforeend', newDiv);
+        if (currentLevel < finalLevel) {
+            createRoot(newDiv, childrenCount, currentLevel + 1, finalLevel);
+        }
+    }
+}
 export function generateTree(childrenCount, level) {
+    const treeDiv = document.createElement('div');
+    treeDiv.className = 'item_1';
+    createRoot(treeDiv, childrenCount, 2, level);
+    return treeDiv;
 }
 
 /*
@@ -26,4 +49,15 @@ export function generateTree(childrenCount, level) {
   Сформированное дерево верните в качестве результата работы функции.
 */
 export function replaceNodes() {
+    const lev = 3,
+        childNum = 2;
+    let treeDiv = generateTree(childNum, lev);
+    for (let i = 0; i < childNum; ++i) {
+        let child = treeDiv.children[i];
+        let newChild = document.createElement('SECTION');
+        newChild.className = 'item_2';
+        newChild.innerHTML = child.innerHTML;
+        child.parentNode.replaceChild(newChild, child);
+    }
+    return treeDiv;
 }
